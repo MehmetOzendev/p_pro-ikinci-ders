6c69f9f0c5770b3b20b8a491df9cbec66d7f3d12
Mehmet Özen <162738627+MehmetOzendev@users.noreply.github.com>
2024-03-15 21:21:04 +0300

    main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import random
+
+semboller = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+parola_uzunlugu = int(input("parolanın uzunluğunu sayı olarak girin:"))
+parola = ""
+karakter = random.choice(semboller)
+
+for i in range(parola_uzunlugu):
+    
+    if parola_uzunlugu == "evet":
+        print(rastgele_parola)
+    else:
+        print("yazdiniğiz kelimeyi kontrol edin,şifre oluşturulamiyor!")
+
